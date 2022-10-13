b27721984c53f92c151ffb89a0c521b825953982
J34nPC24 <jean.cotera@epn.edu.ec>
2022-10-13 16:15:37 -0500

    Trabajo actualizado

--- src/Errores/ListaSinNodos.java
+++
@@ -1,7 +0,0 @@
-package Errores;
-
-public class ListaSinNodos extends Throwable {
-    public ListaSinNodos(String mensaje) {
-        super(mensaje);
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,42 @@
-import Errores.ListaSinNodos;
-
 public class Main {
+    //Ingresar todos los caracteres que vas a usar
+    public static String caracteres = "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_><:;\"\'";
+    public static int posicionesQueSeDesplazaParaCifrar = 20;
     public static void main(String[] args) {
-        //Creamos la rama principal
-        ListaEnlazada ramaActiva = new ListaEnlazada("Rama Principal");
-        try {
-            //Mostramos que la rama esta vacia
-            ramaActiva.gitStatus();
-        } catch (ListaSinNodos error) {
-            System.out.println(error.getMessage());
+        String mensajeCifrado = cifrarCadenaConCifradoCesar("Sigueme en instagram @jeanp_cot 19 :)", posicionesQueSeDesplazaParaCifrar);
+
+        System.out.println(mensajeCifrado);
+
+        System.out.println(descifrarMensaje(mensajeCifrado, posicionesQueSeDesplazaParaCifrar));
+    }
+
+    private static String descifrarMensaje(String mensajeCifrado, int posicionesQueSeDesplaza) {
+        String aux = caracteres.substring(posicionesQueSeDesplaza) + caracteres.substring(0, posicionesQueSeDesplaza);
+        mensajeCifrado = mensajeCifrado.toLowerCase();
+        String mensajeDescifrado = "";
+        for (int i =0; i<mensajeCifrado.length();i++){
+            if (mensajeCifrado.charAt(i) == ' '){
+                mensajeDescifrado += ' ';
+                continue;
+            }
+            mensajeDescifrado += caracteres.charAt(aux.indexOf(mensajeCifrado.charAt(i)));
         }
 
-        try {
-            //Hacemos el primer commit
-            ramaActiva.gitCommit(1);
+        return mensajeDescifrado;
+    }
 
-            //Mostramos la rama con sus nodos
-            ramaActiva.gitStatus();
-
-            //Creamos varios commits
-            ramaActiva.gitCommit(2);
-            ramaActiva.gitCommit(3);
-            ramaActiva.gitCommit(4);
-
-            //Mostramos los nuevos commits
-            ramaActiva.gitStatus();
-
-            //Creamos una rama
-            ramaActiva.gitBranch(new ListaEnlazada("Rama 2"));
-
-            //Enumeramos las ramas
-            ramaActiva.enumerarRamas();
-
-            //Seleccionamos otra rama
-            ramaActiva = ramaActiva.gitCheckout(1);
-
-            //Mostramos los commits de la nueva rama
-            ramaActiva.gitStatus();
-
-            //Creamos un commit en la nueva rama
-            ramaActiva.gitCommit(5);
-            ramaActiva.gitCommit(6);
-
-            //Mostramos como el commit se guardo correctamente
-            ramaActiva.gitStatus();
-
-            //Quitamos el ultimo commit
-            ramaActiva.gitRevert();
-
-            //Mostramos como se borro el ultimo commit
-            ramaActiva.gitStatus();
-
-            //Mostramos que podemos regresar a la anterior rama
-            ramaActiva.enumerarRamas();
-
-            //Seleccionamos la otra rama
-            ramaActiva = ramaActiva.gitCheckout(1);
-
-            //Mostramos que es la rama anterior
-            ramaActiva.gitStatus();
-
-            //Nuevo comando: gitMerge
-            ramaActiva.gitMerge(1);
-
-            ramaActiva.gitStatus();
-
-
-        }catch (ListaSinNodos | NullPointerException error) {
-            System.out.println(error.getMessage());
+    private static String cifrarCadenaConCifradoCesar(String mensaje, int posicionesQueSeDesplaza) {
+        String aux = caracteres.substring(posicionesQueSeDesplaza) + caracteres.substring(0, posicionesQueSeDesplaza);
+        mensaje = mensaje.toLowerCase();
+        String mensajeCifrado = "";
+        for (int i =0; i<mensaje.length();i++){
+            if (mensaje.charAt(i) == ' '){
+                mensajeCifrado += ' ';
+                continue;
+            }
+            mensajeCifrado += aux.charAt(caracteres.indexOf(mensaje.charAt(i)));
         }
 
+        return mensajeCifrado;
     }
 }
